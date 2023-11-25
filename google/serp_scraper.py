e29b9131b397c49b63c460b7a7b94156190e40fd
DEn4Ik <denpodshivalov091@gmail.com>
2023-11-26 00:45:53 +1000

    serp_scraper reworked to google maps

--- google/serp_scraper.py
+++ google/serp_scraper.py
@@ -1,15 +1,15 @@
+import json
 import os
 import sys
 import ssl
 from urllib.parse import quote
 
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 from google.models import WeedShop
 
 
-def serp_search(q: str, bd_login: str, bd_pwd: str):
+def serp_search(url: str, bd_login: str, bd_pwd: str):
     ssl._create_default_https_context = ssl._create_unverified_context
     bd_auth = f'{bd_login}:{bd_pwd}'
     if sys.version_info[0] == 2:
@@ -19,22 +19,24 @@ def serp_search(q: str, bd_login: str, bd_pwd: str):
             request.ProxyHandler(
                 {'http': f'http://{bd_auth}@brd.superproxy.io:22225',
                  'https': f'http://{bd_auth}@brd.superproxy.io:22225'}))
-        page_data = opener.open(f'http://www.google.com/search?q={q}').read()
+        page_data = opener.open(url).read()
     if sys.version_info[0] == 3:
         import urllib.request
         opener = urllib.request.build_opener(
             urllib.request.ProxyHandler(
                 {'http': f'http://{bd_auth}@brd.superproxy.io:22225',
                  'https': f'http://{bd_auth}@brd.superproxy.io:22225'}))
-        page_data = opener.open(f'http://www.google.com/search?q={q}').read()
+        page_data = opener.open(url).read()
     return find_shop_url_in_google_result(page_data)
 
 
-def find_shop_url_in_google_result(res):
-    soup = BeautifulSoup(res, "lxml")
-    rso_div = soup.find('div', {'id': 'rso'})
-    url = rso_div.find_all('a')[0].get('href')
-    return url
+def find_shop_url_in_google_result(res: str):
+    res_json = json.loads(res)
+    link = None
+    if res_json.get('organic'):
+        organic = res_json.get('organic')[0]
+        link = organic.get('link')
+    return link
 
 
 def find_all_shop_urls():
@@ -44,7 +46,11 @@ def find_all_shop_urls():
     shops = WeedShop.objects.filter(store_url__isnull=True)
     for shop in shops:
         q = f'{shop.store_name} {shop.address}'
+        q = 'GREEN WORLD 1901 HARWOOD AVE N UNIT 34'
         url_q = quote(q)
-        store_url = serp_search(url_q, bd_login, bd_pwd)
-        shop.store_url = store_url
-        shop.save()
+        url = 'https://www.google.com/maps/search/' + url_q + '/?gl=us&lum_json=1'
+        store_url = serp_search(url, bd_login, bd_pwd)
+        print(store_url)
+        if store_url:
+            shop.store_url = store_url
+            shop.save()
