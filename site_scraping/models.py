144a9369403ad41e284118a68a8b9a4b6703a0b2
DEn4Ik <denpodshivalov091@gmail.com>
2023-11-25 20:43:39 +1000

    models upd

--- site_scraping/models.py
+++ site_scraping/models.py
@@ -12,8 +12,12 @@ class SiteData(models.Model):
     phone_number = models.CharField(max_length=255, null=True, blank=True)  # can get from stores page / contact page
 
     ecommerce_provider = models.TextField(null=True, blank=True)  # Dutchie, Buddi, Leafly, Weedmaps
-    type_of_delivery_offered = models.TextField(null=True, blank=True)  # Instant delivery, Timeslot delivery, Same-day delivery
 
+    checkout_page = models.CharField(max_length=255, null=True, blank=True)
+    type_of_delivery_offered = models.TextField(null=True, blank=True)  # Instant delivery, Timeslot delivery, Same-day delivery
     delivery_qualifications = models.TextField(null=True, blank=True)  # Minimum order amount, minimum order amount by zone (kilometers)
+    #  it can be offered from checkout pages
+
     minimum_delivery_fee = models.IntegerField(null=True, blank=True)
     zones = models.TextField(null=True, blank=True)
+    # idk how to get it
