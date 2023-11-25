e1bdc3d5b761974dd88f63e0ea58af3d68c8b917
DEn4Ik <denpodshivalov091@gmail.com>
2023-11-25 19:27:20 +1000

    added apps

---
+++ site_scraping/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from google.models import WeedShop
+
+
+class SiteData(models.Model):
+    shop = models.ForeignKey(WeedShop, on_delete=models.CASCADE)
+    dispensary_name = models.TextField()
+    service_options = models.TextField()  # delivery, pickup, curb-site pickup
+    phone_number = models.CharField()
--- weed_shops_scraper/settings.py
+++ weed_shops_scraper/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'google',
+    'site_scraping',
+    'delivery'
 ]
 
 MIDDLEWARE = [
