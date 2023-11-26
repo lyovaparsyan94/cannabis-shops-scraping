169c7635e2d1f83dbe3d0a028dc506cba4813b33
DEn4Ik <denpodshivalov091@gmail.com>
2023-11-26 19:05:49 +1000

    ecommerce research upd

--- site_scraping/scrapers/prod/weedshop_scraper.py
+++ site_scraping/scrapers/prod/weedshop_scraper.py
@@ -8,21 +8,18 @@ from site_scraping.scrapers.base_selen.base_selenium_worker import BaseSeleniumW
 
 
 class ShopBaseScraper(BaseSeleniumWorker):
-    contact_page_vars = [
-        '/stores',
-        '/contact-us/',
-        '/pages/store-locator',
-        '/contact/',
-        '/#contact'
-    ]
-    order_page_vars = [
-        '/order',
-        '/product-menu/'
-    ]
 
-    delivery_page_vars = [
-        ''
-    ]
+    _ecommerce = {
+        #  basically u can see logo in shoppin/order/checkout pages"
+        'Dutchie': '<style>   id="dutchie--embed__styles"',  # if dutchie will be 100% on the order page
+
+        # in footer
+        'weedmaps': '<a rel="noreferrer noopener" href="https://weedmaps.com/dispensaries/cannaverse-scarborough" data-type="URL" data-id="https://weedmaps.com/dispensaries/cannaverse-scarborough" target="_blank">weedmaps.com</a>',
+
+        'buddi': 'div id=buddi-app',  # on shopping page
+
+        'leafly': ''
+    }
 
     def main(self):
         url = 'https://budderscannabis.ca/'
@@ -35,7 +32,8 @@ class ShopBaseScraper(BaseSeleniumWorker):
         # check nav elems text?
 
     def _filter_nav(self, driver: Chrome):
-        navbar = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, "nav")))
+        navbar = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.TAG_NAME, "nav")))  # or header
         a_elems = navbar.find_elements(By.TAG_NAME, 'a')
         for a_elem in a_elems:
             if '' in a_elem.text:
@@ -44,6 +42,13 @@ class ShopBaseScraper(BaseSeleniumWorker):
         for button_elem in button_elems:
             if '' in button_elem.text:
                 pass
+        # check all nav links and check elems
+            # if elem style 'id="dutchie--embed__styles"' on page then = Dutchie
+
+    def _dutchie_checker(driver: Chrome):
+        pass
+        # need to find in nav shop page
+            # if style 'id="dutchie--embed__styles"' precense on page => return True
 
 
 if __name__ == '__main__':
