f270d5b1389f5be01db6e88e237b3f682802f657
DEn4Ik <denpodshivalov091@gmail.com>
2023-12-04 14:08:09 +1000

    added export to xlsx action

--- google/admin.py
+++ google/admin.py
@@ -1,6 +1,7 @@
 import csv
-from django.contrib import admin
 
+import openpyxl
+from django.contrib import admin
 from google.models import WeedShop
 from django.http import HttpResponse
 
@@ -20,9 +21,9 @@ class WeedShopAdmin(admin.ModelAdmin):
     list_display_links = ('pk',)
     search_fields = ['store_name',]
 
-    actions = ['export_csv']
+    actions = ['export_csv', 'export_xlsx']
 
-    def export_csv(modeladmin, request, queryset):
+    def export_csv(modeladmin, request, queryset) -> HttpResponse:
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename=output.csv'
         csv_writer = csv.writer(response, delimiter='|')
@@ -35,4 +36,22 @@ class WeedShopAdmin(admin.ModelAdmin):
             row_rough = get_row(row)
             row = list(row_rough.values())
             csv_writer.writerow(row)
+        return response
+
+    def export_xlsx(modeladmin, request, queryset) -> HttpResponse:
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=output.xlsx'
+        workbook = openpyxl.Workbook()
+        worksheet = workbook.active
+        headers = [
+            'Municipality or First Nation', 'Store Name', 'Address', 'Store Application Status',
+            'website', 'service options', 'phone_number', 'ecommerce provider', 'type of delivery offered',
+            'delivery qualifications', 'minimum delivery fee', 'zones'
+        ]
+        worksheet.append(headers)
+        for shop in queryset:
+            row_rough = get_row(shop)
+            row = list(row_rough.values())
+            worksheet.append(row)
+        workbook.save(response)
         return response
