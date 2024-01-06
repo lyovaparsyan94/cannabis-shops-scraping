39e40d9a5db8f96c0f5eff7ba7997fe664ca621c
lyovaparsyan94 <parsyan.1994@mail.ru>
2024-01-06 22:39:42 +0400

    updates

---
+++ platform_scrapper/src/count_files.py
@@ -0,0 +1,8 @@
+import os
+from pprint import pprint
+path = r"C:\Users\1\OneDrive\Рабочий стол\DOT\cannabis-shops-scraping\platform_scrapper"
+txt_files = [f for f in os.listdir(path) if f.endswith('.txt')]
+num_txt_files = len(txt_files)
+
+print(f"There are {num_txt_files} .txt files in {path}")
+pprint(os.listdir(path))
---
+++ platform_scrapper/utilities/parse_1by1.py
@@ -0,0 +1,7 @@
+# disp = manager.query_maker('623c97081ec27f0099c327ab').get('cName', None)
+# if disp:
+#     manager.scan_and_save(ecom_provider='Dutchie', store="Due North Cannabis",
+#                           shop_address="150 CHURCHILL BLVD UNIT C001", state="SAULT STE. MARIE",
+#                           status="Authorized to Open",
+#                           url="http://duenorthcannabis.com/", index=71, despensary_id=disp,
+#                           service_options="['Curbside pickup', 'Delivery', 'In-store pickup', 'In-store shopping', 'Same-day delivery']")
