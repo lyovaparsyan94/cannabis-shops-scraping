04db4e02c8be9988f3fdacbf3d4074ef55f0ada4
lyovaparsyan94 <parsyan.1994@mail.ru>
2024-01-14 20:37:12 +0400

    save

--- platform_scrapper/utilities/parse_1by1.py
+++ platform_scrapper/utilities/parse_1by1.py
@@ -2,7 +2,7 @@ from platform_scrapper.utilities.manager import Manager
 
 
 manager = Manager()
-disp = manager.query_maker('62019a5684915100a81b0b4d').get('cName', None)
+disp = manager.query_maker('60086c94f8526427d535d1d1').get('cName', None)
 # if disp:
 #     manager.scan_and_save(ecom_provider='Dutchie', store="Due North Cannabis",
 #                           shop_address="150 CHURCHILL BLVD UNIT C001", state="SAULT STE. MARIE",
