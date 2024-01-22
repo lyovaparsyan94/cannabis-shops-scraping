73a9fd07f470d2d9b247ef07b985ba8aa72529e2
lyovaparsyan94 <parsyan.1994@mail.ru>
2024-01-22 11:22:37 +0400

    save

--- platform_scrapper/utilities/file_modifier.py
+++ platform_scrapper/utilities/file_modifier.py
@@ -1,18 +1,21 @@
 import os
 import json
+from os.path import abspath, dirname, join
 
 from platform_scrapper.src.write_reports import clean_data
 
 
 def reporter(file_to_append=None, json_to_read=None, store=None, address=None, del_mode=False, auto=False):
+    src = r'C:\Users\1\OneDrive\Рабочий стол\DOT\cannabis-shops-scraping\platform_scrapper\utilities'
+    dst = r'C:\Users\1\OneDrive\Рабочий стол\DOT\cannabis-shops-scraping\platform_scrapper\CANNABIS_RESULTS\collected_data'
+
     if address and store:
         store1 = str(store).replace(" ", '_').replace("'", '').capitalize()
         address1 = str(address).replace(" ", '_').replace("'", '').capitalize()
         storename = store1 + address1
         file_to_append = None
         print(f'searching {storename} ...')
-        for file_name in os.listdir(
-                r'C:\Users\1\OneDrive\Рабочий стол\DOT\cannabis-shops-scraping\platform_scrapper\utilities'):
+        for file_name in os.listdir(src):
             if file_name.endswith(f"{storename}.txt") and not file_name.startswith('COPY'):
                 print('txt found', file_name)
                 file_to_append = file_name
@@ -43,7 +46,7 @@ def reporter(file_to_append=None, json_to_read=None, store=None, address=None, d
                             file.write(i)
                             continue
                 file.write(line)
-    print(f'{store} {address}.txt')
+    print(f'Current file is {store} {address}.txt')
     os.remove(f"{file_to_append}_G.txt")
     if del_mode:
         if not auto:
@@ -54,5 +57,11 @@ def reporter(file_to_append=None, json_to_read=None, store=None, address=None, d
             print(f'removed {file_to_append} and {json_to_read}')
 
 
-reporter(store="J. Supply Co. Guelph", address='1515 GORDON ST UNIT 106', del_mode=True)
+# reporter(store="TOKE CANNABIS", address='247 CENTENNIAL PKY N UNIT 2', del_mode=False)
 # reporter(file_to_append="38Spark_cannabis_24_toronto_st_n.txt", json_to_read='t_Spark_cannabis_24_toronto_st_n.json', del_mode=True)
+
+# filename = 'TOKE CANNABIS 247 CENTENNIAL PKY N UNIT 2.txt'
+# ROOT_DIR = os.getcwd()
+# CONFIGS_DIR = abspath(dirname(__file__))
+# FILE_DIR =join(CONFIGS_DIR, filename)
+# print(ROOT_DIR)
--- platform_scrapper/utilities/parse_1by1.py
+++ platform_scrapper/utilities/parse_1by1.py
@@ -1,11 +1,9 @@
 from platform_scrapper.utilities.manager import Manager
-import shutil
 manager = Manager()
-disp = manager.query_maker('5fc5762bd9d0e500abb94e2b').get('cName', None)
+disp = manager.query_maker('64833eaca3386b0017a49653').get('cName', None)
 # if disp:
 #     manager.scan_and_save(ecom_provider='Dutchie', store="Due North Cannabis",
 #                           shop_address="150 CHURCHILL BLVD UNIT C001", state="SAULT STE. MARIE",
 #                           status="Authorized to Open",
 #                           url="http://duenorthcannabis.com/", index=71, despensary_id=disp,
 #                           service_options="['Curbside pickup', 'Delivery', 'In-store pickup', 'In-store shopping', 'Same-day delivery']")
-# 60806bc5ece0aa00d28bff8d
