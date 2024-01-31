ed9e7b9599a0b2fff8f4e4c08fbd850bc23cbe0a
lyovaparsyan94 <parsyan.1994@mail.ru>
2024-01-31 19:41:16 +0400

    save

---
+++ platform_scrapper/src/zip_report.py
@@ -0,0 +1,13 @@
+import zipfile
+import os
+
+def zip_files(path_to_files, path_to_zip):
+    with zipfile.ZipFile(path_to_zip, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+        for root, dirs, files in os.walk(path_to_files):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zip_file.write(file_path, os.path.relpath(file_path, path_to_files))
+
+# path_to_files = r"..."
+# path_to_zip = r"...\collected_data.zip"
+# zip_files(path_to_files, path_to_zip)
