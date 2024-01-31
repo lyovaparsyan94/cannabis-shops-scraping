4508a9410cfc9131087c37a98438a2b06c59aa95
lyovaparsyan94 <parsyan.1994@mail.ru>
2024-01-31 19:25:14 +0400

    1400

--- platform_scrapper/utilities/file_modifier.py
+++ platform_scrapper/utilities/file_modifier.py
@@ -55,7 +55,7 @@ def reporter(file_to_append=None, json_to_read=None, store=None, address=None, d
             print(f'removed {file_to_append} and {json_to_read}')
 
 
-# reporter(store="Body and Spirit Cannabis", address='361 YONGE ST', del_mode=False)
+# reporter(store="BLUEBIRD CANNABIS CO. CLARENCE STREET", address='121 CLARENCE ST', del_mode=True)
 # ROOT_DIR = os.getcwd()
 # CONFIGS_DIR = abspath(dirname(__file__))
 # FILE_DIR =join(CONFIGS_DIR, filename)
