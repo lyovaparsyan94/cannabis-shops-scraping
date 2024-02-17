4f5d744fbc30c95eb3f6f091e3b035fca9ead84d
Lyova PArsyan <parsyan.lyova@gmail.com>
2024-02-17 15:35:43 +0400

    save 1710

--- platform_scrapper/utilities/manager.py
+++ platform_scrapper/utilities/manager.py
@@ -224,7 +224,7 @@ class Manager:
                         df.at[index, 'checked'] = True
                     elif ecom_provider in ecommerse_providers and 'Delivery' in type_of_delivery_offered:
                         if ecom_provider == "Buddi":
-                            buddi_params = {'radius': 7, 'fee': 4.5, 'minimum': 25}
+                            buddi_params = {'radius': 5, 'fee': 5, 'minimum': 50}
                         self.scan_area(state=state, store=store, shop_address=address,
                                        despensary_id='', status=status, url=url,
                                        ecom_provider=ecom_provider, service_options=service_options,
