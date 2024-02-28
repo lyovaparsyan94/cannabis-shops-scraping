ee26ed1bfbf244ce53567cbd6e982b0e466af1ef
Lyova PArsyan <parsyan.lyova@gmail.com>
2024-02-29 01:07:20 +0400

    1947 collected shops

--- platform_scrapper/src/manager.py
+++ platform_scrapper/src/manager.py
@@ -201,7 +201,7 @@ class Manager:
                         df.at[index, 'checked'] = True
                     elif ecom_provider in ecommerse_providers and 'Delivery' in type_of_delivery_offered:
                         if ecom_provider in ecommerse_providers and ecom_provider != DUTCHIE:
-                            buddi_params = {'radius': 15, 'fee': 5.0, 'minimum': 25}
+                            buddi_params = {'radius': 5, 'fee': 4.99, 'minimum': 50}
                         self.scan_area(state=state, store=store, shop_address=address,
                                        despensary_id='', status=status, url=url,
                                        ecom_provider=ecom_provider, service_options=service_options,
